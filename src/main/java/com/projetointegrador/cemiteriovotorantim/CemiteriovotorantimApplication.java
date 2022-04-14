afe2dc6ac838e23f5295b5e440319b7094d6f3e4
hickcp <henrique_c_p@outlook.com>
2022-04-13 23:25:36 -0300

    "exclude DataSource autoconfiguration"

--- src/main/java/com/projetointegrador/cemiteriovotorantim/CemiteriovotorantimApplication.java
+++ src/main/java/com/projetointegrador/cemiteriovotorantim/CemiteriovotorantimApplication.java
@@ -2,8 +2,9 @@ package com.projetointegrador.cemiteriovotorantim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CemiteriovotorantimApplication {
 
 	public static void main(String[] args) {
